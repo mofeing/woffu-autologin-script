880ed6647419aeba9d03e0d6b8911b3ae446da0d
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-03-14 22:13:13 +0100

    Rename action

--- woffu/__main__.py
+++ woffu/__main__.py
@@ -1,7 +1,6 @@
 import sys
 from .woffu import Woffu
 from .credentials import load_credentials
-import argparse
 
 
 print("Woffu Autologin Script\n")
@@ -17,20 +16,9 @@ if not woffunator.is_working_day_for_me():
     print("Not a working day. Enjoy!")
     sys.exit(0)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("cmd", help="", choices=["login", "logout"])
-args = parser.parse_args()
-
 try:
-    if args.cmd == "login":
-        woffunator.sign_in()
-        print("Logged in!")
-    elif args.cmd == "logout":
-        raise NotImplementedError("logout")
-        print("Logged out!")
-    else:
-        print(f"Unknown command: {args.cmd}")
-        sys.exit(1)
+    woffunator.sign_in()
+    print("Logged in!")
 except Exception as e:
     print(f"Something went wrong when trying to log you in/out: {e.message}")
     sys.exit(1)
