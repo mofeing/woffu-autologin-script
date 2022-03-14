f68af993c8b380064e7e77a8025b65b414dc486c
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-03-14 20:31:58 +0100

    Add login, logout commands

--- woffu/__main__.py
+++ woffu/__main__.py
@@ -1,6 +1,7 @@
 import sys
 from .woffu import Woffu
 from .credentials import load_credentials
+import argparse
 
 
 print("Woffu Autologin Script\n")
@@ -16,9 +17,20 @@ if not woffunator.is_working_day_for_me():
     print("Not a working day. Enjoy!")
     sys.exit(0)
 
+parser = argparse.ArgumentParser()
+parser.add_argument("cmd", help="", choices=["login", "logout"])
+args = parser.parse_args()
+
 try:
-    woffunator.sign_in()
-    print("Success!")
+    if args["cmd"] == "login":
+        woffunator.sign_in()
+        print("Logged in!")
+    elif args["cmd"] == "logout":
+        raise NotImplementedError("logout")
+        print("Logged out!")
+    else:
+        print(f"Unknown command: {args['cmd']}")
+        sys.exit(1)
 except Exception as e:
     print(f"Something went wrong when trying to log you in/out: {e.message}")
     sys.exit(1)
